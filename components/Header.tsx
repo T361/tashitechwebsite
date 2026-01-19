f3726388fdc5816921eff62ca09df84e6cc2158a
T361 <tshaukat.25@gmail.com>
2026-01-19 23:10:27 +0500

    added logo file

--- components/Header.tsx
+++ components/Header.tsx
@@ -49,7 +49,7 @@ const Header: React.FC<HeaderProps> = ({ currentView, onViewChange }) => {
               transition={{ type: "spring", stiffness: 300, damping: 20 }}
             >
               {/* Inline SVG Logo - No upload required */}
-              <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full h-full">
+              <svg viewBox="0 0 100 100" fill="none" xmlns="logo.png" className="w-full h-full">
                 <defs>
                   <linearGradient id="logoGradient" x1="0" y1="0" x2="100" y2="100" gradientUnits="userSpaceOnUse">
                     <stop stopColor="#0055FF" />
